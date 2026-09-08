589bbab961e711143c507583b8c79c017c54d1c2
vaishnavi382 <vaishnavim974@gmail.com>
2026-09-08 23:26:26 +0530

    Add Home page and update App

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div>
+      <h1>MedConnect</h1>
+      <p>Find the right doctor and book your appointment easily.</p>
+
+      <button>Find a Doctor</button>
+      <button>Book Appointment</button>
+    </div>
+  );
+}
+
+export default Home;
